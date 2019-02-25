8be49ca26fcaddf514ef1e38a4241e01af050fd8
Fabian Sanchez <fasaro@gmail.com>
2019-02-24 19:21:33 -0600

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * Magento 2 Spanish (Spain) language pack
+ *
+ * @category   Language
+ * @package    es_MX
+ * @author     fasaro
+ */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
     'fasaro_es_mx',
