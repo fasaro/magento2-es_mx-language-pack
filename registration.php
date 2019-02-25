664d51d7ed9fadf254315cf057d2acbb5f6bc99f
Fabian Sanchez <fasaro@gmail.com>
2019-02-24 19:15:09 -0600

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'rioxygen_es_mx',
+    'fasaro_es_mx',
     __DIR__
 );
